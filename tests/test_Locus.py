f2dbd13c40a4c6cc2d49f1f3e2144eb27507517f
Rob <schae234@umn.edu>
2016-06-02 16:31:21 -0500

    add stubs for method tests

--- tests/test_Locus.py
+++ tests/test_Locus.py
@@ -7,12 +7,93 @@ from locuspocus import Locus
 def simple_Locus():
     return Locus(1,100,200) 
 
-def test_locus_initialization(simple_Locus):
+def test__initialization(simple_Locus):
     # numeric chromosomes
     assert simple_Locus.chrom == '1'
     assert simple_Locus.start == 100
     assert simple_Locus.end == 200
     assert len(simple_Locus) == 100
+
+def test_as_dict(simple_Locus):
+    assert False
+
+def test_id_getter(simple_Locus):
+    assert False
+
+def test_update(simple_Locus):
+    assert False
+
+def test_as_record(simple_Locus):
+    assert False
+
+def test_from_record(simple_Locus):
+    assert False
+
+def test_setitem(simple_Locus):
+    assert False
+
+def test_getitem(simple_Locus):
+    assert False
+
+def test_update(simple_Locus):
+    assert False
+
+def test_default_getitem(simple_Locus):
+    assert False
+
+def test_start(simple_Locus):
+    assert False
+
+def test_end(simple_Locus):
+    assert False
+
+def test_coor(simple_Locus):
+    assert False
+
+def test_upstream(simple_Locus):
+    assert False
+
+def test_downstream(simple_Locus):
+    assert False
+
+def test_name(simple_Locus):
+    assert False
+
+def test_add(simple_Locus):
+    assert False
+
+def test_eq(simple_Locus):
+    assert False
+
+def test_contains(simple_Locus):
+    assert False
+
+def test_len(simple_Locus):
+    assert False
+
+def test_cmp(simple_Locus):
+    assert False
+
+def test_lt(simple_Locus):
+    assert False
+
+def test_gt(simple_Locus):
+    assert False
+
+def test_sub(simple_Locus):
+    assert False
+
+def test_str(simple_Locus):
+    assert False
+
+def test_repr(simple_Locus):
+    assert False
+
+def test_hash(simple_Locus):
+    assert False
+
+def test_summary(simple_Locus):
+    assert False
 
 #def test_candidate_vs_bootstrap_length(testRefGen,testGWAS):
 #    Term = next(testGWAS.iter_terms())
