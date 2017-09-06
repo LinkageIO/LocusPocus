c7e91a84d87619991f239f371d6b68dfcff0bf7d
Rob <schae234@umn.edu>
2017-09-06 15:33:43 -0500

    remove old installation files

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages, Extension
+from Cython.Distutils import build_ext
 
 import io
 import os
 import re
+import numpy
 
-from setuptools import setup, find_packages, Extension
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        print('Running post-installation setup')
+        check_call('''\
+	pip install -r requirements.txt
+        '''.split())
+        develop.run(self)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        print('Running post-installation setup')
+        check_call('''\
+        pip install -r requirements.txt
+        '''.split())
+        install.run(self)
 
 def read(*names, **kwargs):
     with io.open(
@@ -23,18 +45,36 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+locusdist = Extension(
+    'locuspocus.LocusDist',
+    sources=['locuspocus/LocusDist.pyx'],
+#    extra_compile_args=['-ffast-math'],
+    include_dirs=[numpy.get_include()]
+)
 
 setup(
     name = 'locuspocus',
     version = find_version('locuspocus','__init__.py'),
     packages = find_packages(),
     scripts = [],
-    install_requires = [],
-    package_data = {},
+    ext_modules = [locusdist],
+    cmdclass = {
+        'build_ext' : build_ext,
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+    package_data = {
+        '':['*.cyx']
+    },
+    install_requires = [
+        'cython>=0.16',
+        'numpy>=1.9.1'
+    ],
     include_package_data=True,
+
     author = 'Rob Schaefer',
-    author_email = 'schae234@gmail.com',
+    author_email = 'rob@linkage.io',
     description = 'Genetic coordinates so easy, it seems like MAGIC!',
-    license = "MIT License",
-    url = 'https://github.com/schae234/LocusPocus',
+    license = "Copyright Linkage Analytics 2017. MIT License",
+    url = 'linkage.io',
 )
