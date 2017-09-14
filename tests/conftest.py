2c5f8ac468b94dbbe802bac254711527dc4f8e53
Rob <schae234@umn.edu>
2017-09-14 13:36:39 -0500

    add fixture for Fasta

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ import pytest
 import os
 from locuspocus import Locus
 from locuspocus import Loci
+from locuspocus import Fasta
+
+from locuspocus.Fasta import Chromosome
 
 @pytest.fixture(scope='module')
 def simpleLoci():
@@ -32,4 +35,16 @@ def testRefGen():
         )
     return x
 
-
+@pytest.fixture()                                                               
+def smpl_fasta():                                                                  
+    ''' A simple fasta that agrees with smpl_annot'''                           
+    fasta = Fasta()                                                             
+    chr1 = Chromosome('A'*500000)                                                  
+    chr2 = Chromosome('C'*500000)                                               
+    chr3 = Chromosome('G'*500000)                                               
+    chr4 = Chromosome('T'*500000)                                                  
+    fasta.add_chrom('chr1',chr1)                                                
+    fasta.add_chrom('chr2',chr2)                                                
+    fasta.add_chrom('chr3',chr3)                                                
+    fasta.add_chrom('chr4',chr4)                                                
+    return fasta  
