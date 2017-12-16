a2663fb362991f0d89a7db34fa93fe2264ec4956
Rob <schae234@umn.edu>
2017-12-16 17:53:19 -0600

    change Fasta to be backed by bcolz for chromosomes

--- locuspocus/Fasta.py
+++ locuspocus/Fasta.py
@@ -2,10 +2,13 @@ from collections import defaultdict
 import logging
 
 import re
+import numpy as np
 
 from minus80 import Freezable
 import reprlib
 import pprint
+
+from functools import lru_cache
 
 class Chromosome(object) :                                                          
     '''                                                                             
@@ -28,8 +31,9 @@ class Chromosome(object) :
     12
 
     '''                                                                            
-    def __init__(self,seq):                                                        
-        self.seq = str(seq)                                                        
+    def __init__(self,name,seq):                                                        
+        self.name = name
+        self.seq = seq                                                      
     def __getitem__(self,pos):                                                     
         if isinstance(pos,slice):                                                  
             if pos.start < 1:
@@ -41,14 +45,14 @@ class Chromosome(object) :
                 raise ValueError('Genetic coordinates cannot start less than 1')
             return self.seq[int(pos)-1]                                                
     def __len__(self):                                                             
-        return len(self.seq)        
+        return len(self.seq)         
 
     def __repr__(self):
         return 'Chromosome({})'.format(
             reprlib.repr(self.seq)
         )
 
-class Fasta(object):
+class Fasta(Freezable):
     '''
         A pythonic interface to a FASTA file. This interface
         allows convenient slicing into contigs (chromosomes).
@@ -67,45 +71,7 @@ class Fasta(object):
     log.addHandler(handler)                                                         
     log.setLevel(logging.INFO)          
 
-    def __init__(self):
-        '''
-            Initialize an empty Fasta object.
-        '''
-        self._file = None       
-        self.added_order = []
-        self.chroms = {}
-        self.nicknames = {}
-        self.attributes = defaultdict(list)
-        self._chrom_set = set(self.chroms.keys())
-
-
-    def to_minus80(self,name):
-        '''
-            Store the Fasta Object in the Minus80
-
-            Parameters
-            ----------
-            name : str
-                The name of the store in the  minus80.
-        '''
-        from minus80 import Freezable
-        store = Freezable(name,type=Freezable.guess_type(self))
-        self._initialize_tables(store)
-        cur = store._db.cursor()
-        # Store the file
-        store._dict('_file',self._file)
-        # Store the chroms in added order
-        for i,chrom in enumerate(self.added_order):
-            seq = self.chroms[chrom].seq
-            cur.execute('INSERT OR REPLACE INTO chroms (added_order,chrom,seq) VALUES (?,?,?)',(i,chrom,seq))
-        for nickname,chrom in self.nicknames.items():
-            cur.execute('INSERT OR REPLACE INTO nicknames (nickname,chrom) VALUES (?,?)',(nickname,chrom))
-        for chrom,attrs in self.attributes.items():
-            for attr in attrs:
-                cur.execute('INSERT OR REPLACE INTO attributes (chrom,attribute) VALUES (?,?)',(chrom,attr))
-
-    @classmethod
-    def from_minus80(cls,name):
+    def __init__(self,name):
         '''
             Load a Fasta object from the Minus80.
 
@@ -118,130 +84,25 @@ class Fasta(object):
             -------
             A Fasta object
         '''
-        self = cls()
-        from minus80 import Freezable
-        store = Freezable(name,type=Freezable.guess_type(self))
-        self._file = store._dict('_file')
-        cur = store._db.cursor()
-        for chrom,seq in cur.execute('SELECT chrom,seq FROM chroms ORDER BY added_order ASC'):
-            self.add_chrom(chrom,Chromosome(seq))
-        for nickname,chrom in cur.execute('SELECT nickname,chrom FROM nicknames'):
-            self.nicknames[nickname] = chrom
-        for chrom,attr in cur.execute('SELECT chrom,attribute FROM attributes'):
-            self.add_attribute(chrom,attr)
-        return self
-        
+        super().__init__(name)
+        # Load up from the database
+        self._initialize_tables()
 
-
-    def __contains__(self,item):
+    def _initialize_tables(self):
         '''
-            Returns boolean indicating if a named
-            contig (chromosome) is in the fasta.
+            Initialize the tables for the FASTA class
+
+            NOTE: internal method
         '''
-        if item in self._chrom_set:
-            return True
-        elif item in self.chroms:
-            return True
-        else:
-            return False
-
-    def __getitem__(self,chrom_name):
-        try:
-            if chrom_name in self.chroms:
-                return self.chroms[chrom_name]
-            if chrom_name in self.nicknames:
-                return self.chroms[self.nicknames[chrom_name]]
-        except Exception as e:
-            raise KeyError(
-                '{} not in Fasta: {}'.format(chrom_name,self._file)
-            )
-
-    def add_chrom(self,chrom_name,chromosome):
-        '''
-            Add a chromosome to the Fasta object.
-        '''
-        self.added_order.append(chrom_name)
-        self.chroms[chrom_name] = chromosome
-
-    def add_attribute(self,chrom_name,attr):
-        '''
-            Add an attribute the the Fasta object.
-            Attributes describe chromosomes and 
-            often follow the '>' token in the FASTA file.
-        '''
-        self.attributes[chrom_name].append(attr)
-
-    @classmethod
-    async def from_file_async(cls,fasta_file,nickname=None):
-        async def produce(queue,fasta_file):
-            # add chromosomes to the queue
-            await queue.put(chrom)
-            await queue.put(None)
-
-        async def consume(queue):
-            while True:
-                chrom = await queue.get()
-                if item is None:
-                    break
-                # put chromosomes in the database
-
-
-        loop = asyncio.get_event_loop()
-        queue = asyncio.Queue(loop=loop)
-        loop.run_until_complete(asyncio.gather())
-        loop.close()
-
-
-    @classmethod
-    def from_file(cls,fasta_file,nickname=None):
-        '''
-            Create a Fasta object from a file.
-        '''    
-        self = cls()
-        self._file = fasta_file
-        with open(self._file,'r') as IN: 
-            cur_chrom = None
-            cur_seqs = []
-            for line in IN:
-                line = line.strip()
-                if line.startswith('>'):
-                    # Finish the last chromosome before adding a new one
-                    if cur_chrom:
-                        self.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
-                    name,*attrs = line.lstrip('>').split()
-                    cur_chrom = name
-                    cur_seqs = []
-                    for attr in attrs:
-                        self.add_attribute(cur_chrom,attr)
-                    if nickname != None:
-                        pattern,replace = nickname
-                        alt = re.sub(pattern,replace,line)
-                        if alt != line:
-                            log('Found a nickname: mapping {} -> {}',alt,name)
-                            self.nicknames[alt] = name
-                            self.nicknames[name] = alt
-                else:
-                    cur_seqs.append(line)
-            # Add the last chromosome
-            self.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
-        return self
-
-
-
-
-    @staticmethod
-    def _initialize_tables(store):
-        cur = store._db.cursor()
+        cur = self._db.cursor()
 
         cur.execute('''
-            CREATE TABLE IF NOT EXISTS chroms (
-                added_order INT,
-                chrom TEXT,
-                seq BLOB,
-                PRIMARY KEY(chrom)
+            CREATE TABLE IF NOT EXISTS added_order (
+                aorder INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT
             );
         ''')
-        
+
         cur.execute('''
             CREATE TABLE IF NOT EXISTS nicknames (
                 nickname TEXT,
@@ -260,6 +121,161 @@ class Fasta(object):
             )
         ''')
 
+    def add_chrom(self,name,chrom,cur=None):
+        '''
+            Add a chromosome to the Fasta object.
+
+            Parameters
+            ----------
+            name : str
+                The name of the chromosome
+        '''
+        if cur is None:
+            cur = self._db.cursor()
+        cur.execute(
+            '''
+            INSERT OR REPLACE INTO added_order 
+                (name) 
+            VALUES (?)
+            ''',(name,)
+        )
+        seqarray = np.array(list(chrom.seq))
+        self.log.warn(f'Adding {name}') 
+        self._bcolz_array(name,seqarray)
+        
+
+    @classmethod
+    def from_file(cls,name,fasta_file,nickname=None):
+        '''
+            Create a Fasta object from a file.
+        '''    
+        self = cls(name)
+        with open(fasta_file,'r') as IN, self._db as db: 
+            cur = db.cursor()
+            cur_chrom = None
+            cur_seqs = []
+            for line in IN:
+                line = line.strip()
+                if line.startswith('>'):
+                    # Finish the last chromosome before adding a new one
+                    if cur_chrom:
+                        self.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)),cur=cur)
+                    name,*attrs = line.lstrip('>').split()
+                    cur_chrom = name
+                    cur_seqs = []
+                    for attr in attrs:
+                        self.add_attribute(cur_chrom,attr,cur=cur)
+                    if nickname != None:
+                        pattern,replace = nickname
+                        alt = re.sub(pattern,replace,line)
+                        if alt != line:
+                            self.log('Found a nickname: mapping {} -> {}',alt,name)
+                            self._add_nickname(name,alt,cur=cur)
+                            self._add_nickname(alt,name,cur=cur)
+                else:
+                    cur_seqs.append(line)
+            # Add the last chromosome
+            self.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
+        return self
+
+    def __contains__(self,obj):
+        '''
+           Returns boolean indicating if a named
+           contig (chromosome) is in the fasta.
+        '''
+        if isinstance(obj,Chromosome):
+            obj = obj.name 
+        cur = self._db.cursor()
+        # Check if in chrom names
+        in_added = cur.execute(''' 
+            SELECT COUNT(*) FROM added_order
+            WHERE name = ?
+        ''',(obj,)).fetchone()[0]
+        if in_added == 1:
+            return True
+        # Check if in aliases
+        in_alias = cur.execute('''
+            SELECT COUNT(*) FROM nicknames
+            WHERE nickname = ?
+        ''',(obj,)).fetchone()[0]
+        if in_alias == 1:
+            return True
+        # Otherise its not here
+        return False
+   
+    @lru_cache(maxsize=128)
+    def __getitem__(self,chrom_name):
+       try:
+           seq_array = self._bcolz_array(chrom_name)
+           return Chromosome(chrom_name,seq_array)
+       except Exception as e:
+           pass
+       try:
+           chrom_name = self._get_nickname(chrom_name)
+           return self[chrom_name]
+       except Exception as e:
+           raise KeyError(
+               '{} not in Fasta: {}'.format(chrom_name,self._m80_name)
+           )
+
+    def _add_attribute(self,chrom_name,attr,cur=None):
+        '''
+            Add an attribute the the Fasta object.
+            Attributes describe chromosomes and 
+            often follow the '>' token in the FASTA file.
+
+            Parameters
+            ----------
+            chrom_name : str
+                The name of the chromosome you are adding
+                an attribute to
+            attr : str
+                the attribute you are adding
+        '''
+        if cur is None:
+            cur = self._db.cursor()
+        cur.execute(
+            '''
+            INSERT OR REPLACE INTO attributes 
+                (chrom,attribute) 
+             VALUES (?,?)
+             ''',
+            (chrom,attr)
+        )
+
+    def _add_nickname(self,chrom,nickname,cur=None):
+        '''
+            Add a nickname for a chromosome
+
+            Parameters
+            ----------
+            chrom : str
+                The chromosome you want to nickname
+            nickname : str
+                The alternative name for the chromosome
+        '''
+        if cur is None:
+            cur = self._db.cursor()
+        cur.execute(
+            '''
+            INSERT OR REPLACE INTO nicknames 
+                (nickname,chrom) 
+            VALUES (?,?)
+            ''',
+            (nickname,chrom)
+        )
+
+    def _get_nickname(self,nickname):
+        '''
+            Get a chromosomem name by nickname
+
+
+
+        '''
+        return self._db.cursor().execute('''
+            SELECT chrom FROM nicknames 
+            WHERE nickname = ?
+        ''',(nickname,)).fetchone()[0]
 
     def __repr__(self):
         return pprint.saferepr(
