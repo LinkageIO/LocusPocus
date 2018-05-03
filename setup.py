9d715d5ffc61256c71189f0fed8a8446607e3235
Rob <schae234@umn.edu>
2018-05-03 15:53:57 -0500

    add more setup.py info for pypi

--- setup.py
+++ setup.py
@@ -6,18 +6,6 @@ import io
 import os
 import re
 
-from setuptools.command.develop import develop
-from subprocess import check_call
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        print('Running post-installation setup')
-        check_call('''\
-	    pip install -r requirements.txt
-        '''.split())
-        develop.run(self)
-
 def read(*names, **kwargs):
     with io.open(
         os.path.join(os.path.dirname(__file__), *names),
@@ -41,6 +29,40 @@ locusdist = Extension(
 setup(
     name = 'locuspocus',
     version = find_version('locuspocus','__init__.py'),
+
+    description = 'Genetic coordinates made easy!',
+    url = 'http://linkage.io',
+    author = 'Rob Schaefer',
+    author_email = 'rob@linkage.io'
+    license = "Copyright Linkage Analytics 2016. Available under the MIT License",
+
+    classifiers=[
+	# How mature is this project? Common values are
+	#   3 - Alpha
+	#   4 - Beta
+	#   5 - Production/Stable
+	'Development Status :: 4 - Beta',
+
+	# Indicate who your project is intended for
+	'Intended Audience :: Developers',
+	'Topic :: Software Development :: Build Tools',
+
+	# Pick your license as you wish (should match "license" above)
+	 'License :: OSI Approved :: MIT License',
+
+	# Specify the Python versions you support here. In particular, ensure
+	# that you indicate whether you support Python 2, Python 3 or both.
+	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.6',
+    ],
+    keywords='data genetics biology coordinates', 
+    project_urls={
+        'Documentation' : 'http://linkage.io',
+        'Source' : 'https://github.com/LinkageIO/LocusPocus',
+        'Tracker' : 'https://github.com/LinkageIO/LocusPocus/issues'
+    },
+
+
     packages = find_packages(),
     scripts = [],
     ext_modules = [locusdist],
@@ -63,9 +85,4 @@ setup(
     ],
     include_package_data=True,
 
-    author = 'Rob Schaefer',
-    author_email = 'rob@linkage.io',
-    description = 'Genetic coordinates so easy, it seems like MAGIC!',
-    license = "Copyright Linkage Analytics 2017. MIT License",
-    url = 'linkage.io',
 )
