87d9c3eb28ed6eb2d0daf53aef099b6814b599e9
Rob <rob@linkage.io>
2019-06-12 17:12:15 -0600

    hook locus up to db

--- locuspocus/Locus.py
+++ locuspocus/Locus.py
@@ -2,81 +2,126 @@
 from collections import defaultdict
 from dataclasses import dataclass,field,InitVar,FrozenInstanceError
 from itertools import chain
-from typing import Union, Any, List, Optional, cast, Callable, Iterable
+from typing import Union, Any, List, Optional, cast, Callable, Iterable, Dict
 
 from .Exceptions import StrandError, ChromosomeError
 
 import re
 import math
-import hashlib
 import locuspocus
 import dataclasses
+import uuid
 
 import pandas as pd
 import numpy as np
 
+from locuspocus.Loci import Loci
+
+
 __all__ = ['Locus']
 
-class SubLoci():
-    # A restricted list interface to subloci
-    def __init__(self,loci=None):
-        if loci is None:
-            loci = list()
-        self._loci = loci
-
-    def __iter__(self):
-        return (x for x in self._loci)
-
-    def add(self,locus):
-        self._loci.append(locus)
-
-    def __getitem__(self,index):
-        return self._loci[index]
-
-    def __len__(self):
-        return len(self._loci)
-    
-
-class LocusAttrs():
-    # a restricted dict interface to attributes
-    def __init__(self,attrs=None):
-        if attrs is None:
-            attrs = dict()
-        self._attrs = attrs
-
-    def keys(self):
-        return self._attrs.keys()
-
-    def values(self):
-        return self._attrs.values()
-
-    def items(self):
-        return self._attrs.items()
-
-    def __getitem__(self,key):
-        return self._attrs[key]
-
-    def __setitem__(self,key,val):
-        self._attrs[key] = val
-
-    def __repr__(self):
-        return repr(self._attrs)
-
-@dataclass()
 class Locus:
-    chromosome: str
-    start: int 
-    end: int
+    
+    _ref = Loci()
+    
+    def __init__(
+        self,
+        chromosome: str,
+        start: int,
+        end: int,
 
-    source: str = 'locuspocus'
-    feature_type: str = 'locus'
-    strand: str = '+'
-    frame: int = None
-    name: str = None
+        source: str = 'locuspocus',
+        feature_type: str = 'locus',
+        strand: str = '+',
+        frame: int = None,
+        name: str = None,
 
-    # Extra locus stuff
-    attrs: LocusAttrs = field(default_factory=LocusAttrs)
-    subloci: SubLoci = field(default_factory=SubLoci) 
+        # Extra locus stuff
+        attrs: dict = None,
+        parent = None,
+        children = None
+    ):
+        if (chromosome,start,end) == (None,None,None):
+            # This allows Locus objects to be created by class methods 
+            self._LID = name
+        else:
+            # Add the info to the database
+            LID = self._ref.add_locus(
+                chromosome,
+                start,
+                end,
+                source,
+                feature_type,
+                strand,
+                frame,
+                name,
+                attrs,
+                parent,
+                children
+            )
+            self._LID = LID
+
+    def __del__(self):
+        '''
+            Deletes a locus object, inlcuding any traces 
+            of it from the Loci db
+        '''
+        self._ref.del_locus(self)
+
+    def _core_property(self,key):
+        val, = self._ref._db.cursor().execute(f'''
+            SELECT {key} FROM loci WHERE LID = ?
+        ''',(self._LID,)
+        ).fetchone()
+        return val
+
+    @property
+    def chromosome(self):
+        return self._core_property('chromosome')
+        
+    @property
+    def start(self):
+        return self._core_property('start')
+
+    @property
+    def end(self):
+        return self._core_property('end')
+
+    @property
+    def feature_type(self):
+        return self._core_property('feature_type')
+
+    @property
+    def strand(self):
+        return self._core_property('strand')
+
+    @property
+    def frame(self):
+        return self._core_property('frame')
+
+    @property
+    def name(self):
+        return self._core_property('name') 
+
+    @property
+    def parent(self):
+        parent_lid, = self._ref._db.cursor('''
+            SELECT parent FROM relationships WHERE child = ?
+        ''',(self._LID))
+        return self.from_LID(parent_LID)
+
+    @parent.setter
+    def parent(self,val):
+        raise NotImplementedError
+
+    @property
+    def children(self):
+        raise NotImplementedError
+
+    @children.setter
+    def children(self,val):
+        raise NotImplementedError
+
 
     def __len__(self):
         return abs(self.end - self.start) + 1
@@ -123,37 +168,6 @@ class Locus:
         else:
             return self.chromosome > locus.chromosome
 
-    def __hash__(self):
-        '''
-            Convert the locus to a hash, uses md5. The hash
-            is computed using the primary
-
-            Parameters
-            ----------
-            None
-
-            Returns
-            -------
-            int : md5 hash of locus
-
-        '''
-        field_list = [str(x) for x in (
-            self.chromosome,
-            self.start,
-            self.end,
-            self.source,
-            self.feature_type,
-            self.strand,
-            self.frame,
-            self.name
-        )]
-        subloci_list = [str(hash(x)) for x in self.subloci]
-        #attr_list = list(chain(*zip(map(str,self.attrs.keys()),map(str,self.attrs.values()))))
-        # Create a full string
-        loc_string  = "_".join(field_list + subloci_list) # attr_list + subloci_list)
-        digest = hashlib.md5(str.encode(loc_string)).hexdigest()
-        return int(digest, base=16)
-
     @property
     def stranded_start(self):
         if self.strand == '+':
@@ -178,23 +192,6 @@ class Locus:
     def __setitem__(self,key,val):
         self.attrs[key] = val
 
-    def add_sublocus(self,locus):
-        self.subloci.add(locus)
-
-    def as_record(self):
-        return ((
-            self.chromosome,
-            self.start,
-            self.end,
-            self.source,
-            self.feature_type,
-            self.strand,
-            self.frame,
-            self.name,
-            hash(self)
-        ),
-            self.attrs
-        )
 
     def default_getitem(self,key,default=None) -> Any:
         '''
@@ -365,3 +362,13 @@ class Locus:
 
     def __str__(self):
         return repr(self)
+
+    @classmethod
+    def from_LID(cls,LID,ref=None):
+        # Create a locus using sentinels
+        l = cls(None,None,None,name=LID)
+        # assign ref if needed
+        if ref is not None:
+            l._ref = ref
+        return l
+
