bb97fff017e6e9495717e3798a121323432f8af5
Rob <rob@linkage.io>
2019-06-14 17:42:46 -0600

    black out so we can debug

--- locuspocus/__init__.py
+++ locuspocus/__init__.py
@@ -2,9 +2,10 @@ __version__ = '0.2.0'
 __all__ = ['Locus','RefLoci','Fasta']
 
 
-from locuspocus.Chromosome import Chromosome
-from locuspocus.Fasta import Fasta
+#from .chromosome import Chromosome
+#from .fasta import Fasta
 
-from locuspocus.Loci import Loci
-from locuspocus.Locus import Locus
+#from .loci import Loci
+#from .locus import Locus
+
 
