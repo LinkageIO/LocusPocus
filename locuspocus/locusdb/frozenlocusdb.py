a428b98997f77a2a984fe907217db2fc77f73f57
Rob <rob@linkage.io>
2019-09-24 14:40:55 -0600

    allow frozen loci

---
+++ locuspocus/locusdb/frozenlocusdb.py
@@ -0,0 +1,10 @@
+from minus80 import Freezable
+
+from .locusdbmixin import LocusDBMixin
+
+class FrozenLocusDB(LocusDBMixin, Freezable):
+
+    def __init__(self,name,basedir=None):
+        super().__init__(name,basedir=basedir)
+        # Ititialize the minus80 API
+        self._db = self.m80.db
