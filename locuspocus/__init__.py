3a56a8baef7d7ab52826121df8ed8ab918b15280
rob <rob@linkage.io>
2020-02-14 10:18:17 -0700

    put logging singleton in __init__

--- locuspocus/__init__.py
+++ locuspocus/__init__.py
@@ -2,6 +2,7 @@ __version__ = '1.0.2'
 
 __all__ = ['Locus','RefLoci','Fasta']
 
+import logging
 
 from .chromosome import Chromosome
 from .fasta import Fasta
@@ -10,3 +11,15 @@ from .locus import Locus
 from .loci import Loci
 
 from .term import Term
+
+log = logging.getLogger('locuspocus')
+# One of DEBUG, INFO, WARNING, ERROR, CRITICAL
+log.setLevel(logging.INFO)
+# Set up the console handler
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
+ch.setFormatter(formatter)
+ch.setLevel(logging.INFO)
+log.addHandler(ch)
+
+
