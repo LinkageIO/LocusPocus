28b5febae2d9de09e27d1282ea6889bb1ebbc36e
rob <rob@linkage.io>
2020-02-19 11:54:23 -0700

    require python 3.8

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
 	# Specify the Python versions you support here. In particular, ensure
 	# that you indicate whether you support Python 2, Python 3 or both.
 	'Programming Language :: Python :: 3',
-	'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.8',
     ],
     keywords='data genetics biology coordinates', 
     project_urls={
@@ -66,6 +66,7 @@ setup(
     package_data = {
         '':['*.cyx']
     },
+    python_requires='>=3.8',
     setup_requires = [
         # Setuptools 18.0 properly handles Cython extensions.
         'setuptools>=18.0',
